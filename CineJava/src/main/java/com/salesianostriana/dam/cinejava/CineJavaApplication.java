3cda743c33bd5562c35b687d88e0597638850dcf
Alejandro Jiménez Fernández <jimenezalejandro505@gmail.com>
2023-04-20 12:58:14 +0200

    Añadido el trabajo java y paginas de html

---
+++ CineJava/src/main/java/com/salesianostriana/dam/cinejava/CineJavaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.cinejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineJavaApplication.class, args);
+	}
+
+}
---
+++ CineJava/src/test/java/com/salesianostriana/dam/cinejava/CineJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.cinejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
