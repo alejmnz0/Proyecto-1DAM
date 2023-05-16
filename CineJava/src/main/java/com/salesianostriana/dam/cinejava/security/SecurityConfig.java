286aaf75f256955a5f2928ae0f601d498ad11ba8
Alejandro Jiménez Fernández <jimenezalejandro505@gmail.com>
2023-05-16 14:21:23 +0200

    Insert.sql modificado, pequeños cambios en la pagina

--- CineJava/src/main/java/com/salesianostriana/dam/cinejava/security/SecurityConfig.java
+++ CineJava/src/main/java/com/salesianostriana/dam/cinejava/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 		.authorizeRequests()
-			.antMatchers("/css/**","/js/**", "/h2-console/**").permitAll()
+			.antMatchers("**/css/**","/js/**", "/h2-console/**", "/register/**").permitAll()
 			.antMatchers("/admin/**").hasRole("ADMIN")
 			.anyRequest().authenticated()
 			.and()
