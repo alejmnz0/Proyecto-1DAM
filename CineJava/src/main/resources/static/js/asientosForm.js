8c546778a8cc9692922e3ec4c77e55d3ece95178
Alejandro Jiménez Fernández <jimenezalejandro505@gmail.com>
2023-06-02 12:26:09 +0200

    cambios en javascript y comienzo de pdf generator

--- CineJava/src/main/resources/static/js/asientosForm.js
+++ CineJava/src/main/resources/static/js/asientosForm.js
@@ -1,7 +1,7 @@
 function submitForm() {
-    var checkboxes = document.getElementsByClassName("asiento-checkbox");
-    var asientosSeleccionados = [];
-    for (var i = 0; i < checkboxes.length; i++) {
+    let checkboxes = document.getElementsByClassName("asiento-checkbox");
+    let asientosSeleccionados = [];
+    for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             asientosSeleccionados.push(checkboxes[i].id);
         }
@@ -9,10 +9,10 @@ function submitForm() {
     document.getElementById("asientosSeleccionadosInput").value = asientosSeleccionados;
 }
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
 })
-var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+let popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
     container: 'body'
 })
